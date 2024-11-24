6f2844de0734cc9f98816b0f9aa68becb90e6a22
damiru2 <chalkarovd11@gmail.com>
2024-11-24 15:52:03 +0500

    add askForInfo function

--- IContact/Controller/ContactsViewController.swift
+++ IContact/Controller/ContactsViewController.swift
@@ -12,18 +12,24 @@ class ContactsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addActionPressed(_ sender: Any) {
+        askForInfo()
     }
-    */
+    
+    func askForInfo() {
+        let alertController = UIAlertController(title: "Add Contact", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        alertController.addTextField()
+        
+        let saveAction = UIAlertAction(title: "Add", style: .default)
+        alertController.addAction(saveAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+    }
+   
 
 }
